50c884fb90bd34231e0cabc90f8f8a9703521aab
santosh <coderpsk@gmail.com>
2017-12-04 11:04:47 +0530

    wrote omplete component and its logic and data bind with directives

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,7 +1,16 @@
 import {Component } from '@angular/core';
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+    <h2>{{title}}</h2>
+    <ul>
+    <li *ngFor = "let course of courses">
+    {{ course }}
+    <li>
+    </ul>
+    `
 })
 export class CoursesComponent {
+title = "list of courses:";
+courses = ["course1","course2","course2"]
     }
